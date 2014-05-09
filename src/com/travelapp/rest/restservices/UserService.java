bc6d34447a670a8f7a1300e332253516cda07720
pat2013 <nan.yu.pat@gmail.com>
2014-05-09 13:43:48 -0700

    few Change

--- src/com/travelapp/rest/restservices/UserService.java
+++ src/com/travelapp/rest/restservices/UserService.java
@@ -2,12 +2,21 @@ package com.travelapp.rest.restservices;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import com.travelapp.rest.beans.UserBean;
 import com.travelapp.rest.processes.UserProcess;
@@ -39,7 +48,7 @@ public class UserService {
 		}
 		
 		// edit a user
-		@POST
+		@PUT
 		@Path("edit")
 		@Produces(MediaType.APPLICATION_JSON)
 		public UserBean editUser(
